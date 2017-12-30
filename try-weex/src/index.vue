1eeb1ed18ae09bf722f7d9f28b9415731cace21a
beautifulgirlxuxu <804316947@qq.com>
2017-12-30 21:31:01 +0800

    weex初体验

---
+++ try-weex/src/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app-wrapper">
+    <home-header></home-header>
+    <!-- <top-channel></top-channel> -->
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<style>
+  /* 全局样式 */
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+    color: #333;
+  }
+</style>
+<style scoped>
+  .app-wrapper {
+    background-color: #f4f4f4;
+  }
+  .r-box {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+</style>
+<script>
+  import util from './assets/util';
+  import Header from './assets/components/Header.vue';
+  import tabBar from './assets/components/tabBar.vue';
+  export default {
+    // 组件
+    "components": {
+      'home-header': Header,
+      'tab-bar': tabBar
+    },
+    // 生命周期，组件创建
+    created () {
+      util.initIconFont();
+    },
+    data: {
+      
+    },
+    methods: {
+      
+    }
+  }
+</script>
